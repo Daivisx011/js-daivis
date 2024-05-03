fcffb5cbcc45ab3baab8e333fd7c67f4e66e73b8
Daivison <167913674+Daivisx011@users.noreply.github.com>
2024-05-02 21:07:15 -0300

    adicionando html, js e css

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let cor;
+let circuloX; // horizontal
+let circuloY; // vertical
+
+function setup() {
+  createCanvas(400, 400);
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  
+  circuloX = [0, 0, 0];
+  circuloY = [random(height), random(height), random(height)];
+}
+
+function draw() {
+  
+  fill(cor);
+  
+  for(let contador in circuloX) {
+    circle(circuloX[contador], circuloY[contador], 50);    
+    circuloX[contador]+= random(0,3);
+    circuloY[contador]+= random(-3,3); 
+    
+    if(circuloX[contador] >= width){
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
